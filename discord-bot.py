66c85dd7381a3e47b21c16ee751807af682cdaf9
THE0_C <theocouvaras@gmail.com>
2023-10-04 08:46:25 +0100

    removed something

--- discord-bot.py
+++ discord-bot.py
@@ -2,13 +2,28 @@ import discord
 import requests
 from discord.ext import commands, tasks
 import datetime
-import asyncio
 utc = datetime.timezone.utc
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.tree.command(name="channel", description ="Sets the channel to send the message")
+async def ch(ctx: discord.Interaction, channel: discord.TextChannel):
+    global C 
+    C = discord.utils.get(ctx.guild.channels, name=str(channel))
+    await ctx.response.send_message(content="channel changed to: {y}".format(y=channel))
+
+@bot.tree.command(name="test")
+async def test(ctx: discord.Interaction):
+    response = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": "2D1xuvyksKFdFHCa8a4cUPHl5q0O8YKDhvSL7CUO"})
+    iurl = response.json()['url']
+    exp = response.json()['explanation']
+    img = discord.Embed().set_image(url=iurl)
+    message_channel = bot.get_channel(C.id)
+    await message_channel.send(content=exp, embed=img)
 
 
 @bot.tree.command(name="iotd", description ="Shows the NASA Image of the Day")
@@ -25,7 +40,7 @@ async def called_once_a_day():
     iurl = response.json()['url']
     exp = response.json()['explanation']
     img = discord.Embed().set_image(url=iurl)
-    message_channel = bot.get_channel(1151160051193626725)
+    message_channel = bot.get_channel(C.id)
     await message_channel.send(content=exp, embed=img)
 
 @called_once_a_day.before_loop
@@ -39,4 +54,4 @@ async def on_ready():
     await bot.tree.sync()
 
 
-bot.run('MTE0Mjg4NTg2MzY0MzE3MzA0NA.GfxvFX.xti1nMbQd4-Zbg9kPfKxE8xbcYTo5smJA-mNM4')
+bot.run('your api key')
