f9a5feda43f0fd7f5e7e9ac87084e5286981df9d
THE0_C <theocouvaras@gmail.com>
2024-01-10 13:19:01 +0000

    made more compatible

--- discord-bot.py
+++ discord-bot.py
@@ -1,30 +1,15 @@
 import discord
 import requests
 from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions, MissingPermissions
 import datetime
+import json
 utc = datetime.timezone.utc
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
-
-
-@bot.tree.command(name="channel", description ="Sets the channel to send the message")
-async def ch(ctx: discord.Interaction, channel: discord.TextChannel):
-    global C 
-    C = discord.utils.get(ctx.guild.channels, name=str(channel))
-    await ctx.response.send_message(content="channel changed to: {y}".format(y=channel))
-
-@bot.tree.command(name="test")
-async def test(ctx: discord.Interaction):
-    response = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": "2D1xuvyksKFdFHCa8a4cUPHl5q0O8YKDhvSL7CUO"})
-    iurl = response.json()['url']
-    exp = response.json()['explanation']
-    img = discord.Embed().set_image(url=iurl)
-    message_channel = bot.get_channel(C.id)
-    await message_channel.send(content=exp, embed=img)
-
 
 @bot.tree.command(name="iotd", description ="Shows the NASA Image of the Day")
 async def iotd(ctx: discord.Interaction):
@@ -34,14 +19,20 @@ async def iotd(ctx: discord.Interaction):
     img = discord.Embed().set_image(url=iurl)
     await ctx.response.send_message(content=exp, embed=img)
 
-@tasks.loop(time=datetime.time(hour=1, minute=1))
+@tasks.loop(time=datetime.time(hour=6, minute=1))
 async def called_once_a_day():
     response = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key": "2D1xuvyksKFdFHCa8a4cUPHl5q0O8YKDhvSL7CUO"})
     iurl = response.json()['url']
     exp = response.json()['explanation']
     img = discord.Embed().set_image(url=iurl)
-    message_channel = bot.get_channel(C.id)
-    await message_channel.send(content=exp, embed=img)
+    for guild in bot.guilds:
+        for text_ch in guild.text_channels:
+            if  text_ch.name == "nasa-image-of-the-day":
+                message_channel = bot.get_channel(text_ch.id)
+
+                await message_channel.send(content=exp, embed=img)
+            else:
+                pass
 
 @called_once_a_day.before_loop
 async def before():
@@ -54,4 +45,4 @@ async def on_ready():
     await bot.tree.sync()
 
 
-bot.run('your api key')
+bot.run('MTE0Mjg4NTg2MzY0MzE3MzA0NA.GfxvFX.xti1nMbQd4-Zbg9kPfKxE8xbcYTo5smJA-mNM4')
